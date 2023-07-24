96a0293af827b9fbdf4ff286b5f7ee77e6df4374
Vasubhalodi011 <134709721+Vasubhalodi011@users.noreply.github.com>
2023-07-24 12:42:16 +0530

    Add files via upload

---
+++ The new era/2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class time
+{
+        int hr,min,sec;
+        public:
+                void get()
+                {
+                    cout<<"\nEnter Hour :: ";
+                        cin>>hr;
+                        cout<<"Enter Minutes :: ";
+                        cin>>min;
+                        cout<<"Enter Seconds :: ";
+                        cin>>sec;
+                }
+
+                void set()
+                {
+                        cout<<"[ "<<hr<<":"<<min<<":"<<sec<<" ] \n";
+
+                }
+                void sum(time &,time &);
+};
+
+void time::sum(time &t1,time &t2)
+{
+        sec = t1.sec+t2.sec;
+        min = sec/60;
+        sec = sec%60;
+        min = min+t1.min+t2.min;
+        hr = min/60;
+        min = min%60;
+        hr = hr+t1.hr+t2.hr;
+}
+
+int main()
+{
+        time t1,t2,t3;
+        cout<<"\nEnter 1st time Details :: \n";
+        t1.get();
+        cout<<"\nEnter 2nd time Details :: \n";
+        t2.get();
+        cout<<"\nThe 1st time entered is :: ";
+        t1.set();
+        cout<<"\nThe 2nd time entered is :: ";
+        t2.set();
+        
+        t3.sum(t1,t2);
+
+        cout<<"\nThe Sum of two times are :: ";
+
+        t3.set();
+
+        return 0;
+}
