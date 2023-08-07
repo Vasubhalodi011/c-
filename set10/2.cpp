8aedb7f7e0995d1d555fe24c590b39bfd971298d
Vasubhalodi011 <134709721+Vasubhalodi011@users.noreply.github.com>
2023-08-07 13:56:41 +0530

    Add files via upload

---
+++ set10/2.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+    
+	int a,b,c;
+       cout<<"Enter Value A = ";
+	   cin>>a;
+       cout<<"Enter Value B = ";
+	   cin>>b;
+    
+	char solution[100] = "Can not Divide by Zero";
+
+    try{
+
+        if (b==0)
+		{
+             
+			throw 10.69;
+        }
+        else
+	    {
+             
+			c=a/b;
+            cout<<"C = "<<c<<endl;
+        }
+        
+    }
+    catch(...) 
+	{
+            
+			cout<<solution<<endl;
+    } 
+    
+    return 0;
+
+}
